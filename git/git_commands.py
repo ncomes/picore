f6dafb2b5ea7ddf1ee32b314ae2aa6d502d490bd
ncomes <nathancomes@gmail.com>
2020-01-28 19:34:31 -0800

    Add files via upload

--- git/git_commands.py
+++ git/git_commands.py
@@ -1,5 +1,6 @@
-import github
-
+#import github
+import git
+import os
 # or using an access token
 #g = Github("access_token")
 
@@ -11,4 +12,30 @@ def get_repos(username, password):
 	git_info = g.get_users().get_repros()
 	repos = [x.name for x in git_info]
 	return repos
+
+def git_pull(git_dir):
+	#g = git.cmd.Git(git_dir)
+	#g.pull()
+
+	if os.path.isdir(git_dir):
+		repo = git.Repo(git_dir)
+		repo.git.pull('origin', 'master')
+
+	return
+
+def git_commit(git_dir):
+	repo = git.Repo(git_dir)
+	repo.config_writer().set_value('user', 'name', 'ncomes').release()
+	repo.config_writer().set_value('user', 'email', 'nathancomes@gmail.com').release()
+	#repo.config_writer().set_value('user', 'password', 'nate3022').release()
+
+	for file in repo.untracked_files:
+		repo.git.add(file)
+
+	repo.git.commit('-m', 'Auto Commit')
+	repo.git.push('origin', 'master')
+	return
+
+GIT_DIR = r'/home/pi/picore'
+git_commit(GIT_DIR)
 
