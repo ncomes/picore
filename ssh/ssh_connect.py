2b2111361090a6df638b877f9cef76635656027d
ncomes <nathancomes@gmail.com>
2020-01-31 20:43:19 -0800

    Add files via upload

--- ssh/ssh_connect.py
+++ ssh/ssh_connect.py
@@ -1,20 +1,36 @@
 import os
 import paramiko
 
+class SSHClient:
+    def __init__(self):
+        self.ssh = paramiko.SSHClient
 
+    def open_connection(self, hostname, username, pswd):
+        self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        ssh.connect(hostname=hostname, username=username, password=pswd)
+
+    def send_file(self, localpath, remotepath):
+        sftp = self.ssh.open_sftp()
+        sftp.put(localpath, remotepath)
+        sftp.close()
+        return
+
+    def close_connection(self):
+        self.ssh.close()
+        return
+
+'''  
 def ssh_send_file(localpath, remotepath, username, password):
     ssh = paramiko.SSHClient()
 
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    #ssh.load_system_host_keys()
-    #ssh.load_host_keys(os.path.expanduser(os.path.join('~', '.ssh', 'known_hosts')))
     ssh.connect(hostname='piDepot01.local', username=username, password=password)
     sftp = ssh.open_sftp()
     sftp.put(localpath, remotepath)
     sftp.close()
     ssh.close()
     return
-
-local_file = r'/home/pi/Desktop/test.txt'
-remote_path = r'/home/pi/Desktop/test.txt'
-ssh_send_file(local_file, remote_path, 'pi', 'piDepot')
+'''
+#local_file = r'/home/pi/Desktop/test.txt'
+#remote_path = r'/home/pi/Desktop/test.txt'
+#ssh_send_file(local_file, remote_path, 'pi', 'piDepot')
