14438b0f96f9b6b2a7fe24dce9f83af42e941402
ncomes <nathancomes@gmail.com>
2020-02-02 16:03:06 -0800

    Add files via upload

--- server/pi_cam_server.py
+++ server/pi_cam_server.py
@@ -68,7 +68,7 @@ def get_ip_address():
 def send_file(file_name):
 	full_file_name = os.path.join(PHOTO_PATH, file_name)
 	ssh_client.ssh_send_file(full_file_name, full_file_name, 'pi', 'raspberry')
-	call('sudo rm ' + full_file_name)
+	#call('sudo rm ' + full_file_name)
 	#ssh = ssh_client.SSHClient()
 	#ssh.open_connection(hostname='piDepot01.local',
 	#					username='pi',
